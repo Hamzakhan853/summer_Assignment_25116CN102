d6b46226347e258b7d61b3a3b566461f73de3cb0
Hamza Khan <hamzakhan@192.168.1.21>
2026-06-04 21:45:56 +0530

    updated day 4

---
+++ Day_4/Q14.java
@@ -0,0 +1,25 @@
+package Day_4;
+
+import java.util.Scanner;
+public class Q14 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int a = 0, b = 1;
+
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            int next = a + b;
+            a = b;
+            b = next;
+        }
+
+        System.out.println(b);
+    }
+}
---
+++ Day_4/Q16.java
@@ -0,0 +1,31 @@
+package Day_4;
+
+import java.util.Scanner;
+
+public class Q16 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int start = sc.nextInt();
+        int end = sc.nextInt();
+
+        for (int num = start; num <= end; num++) {
+
+            int original = num;
+            int temp = num;
+
+            int digits = String.valueOf(num).length();
+
+            int sum = 0;
+
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += Math.pow(digit, digits);
+                temp /= 10;
+            }
+
+            if (sum == original)
+                System.out.print(num + " ");
+        }
+    }
+}
