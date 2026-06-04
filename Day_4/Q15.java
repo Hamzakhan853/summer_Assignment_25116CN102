83eff15f336d9eda7fd73cdc64a34a3caf149a31
Hamza Khan <hamzakhan@192.168.1.21>
2026-06-04 22:00:08 +0530

    updated day 4

---
+++ Day_4/Q15.java
@@ -0,0 +1,20 @@
+package Day_4;
+import java.util.Scanner;
+public class Q15 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int original = num;
+        int digits = String.valueOf(num).length();
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            sum += Math.pow(digit, digits);
+            num /= 10;
+        }
+        if (sum == original)
+            System.out.println("Armstrong Number");
+        else
+            System.out.println("Not Armstrong Number");
+    }
+}
