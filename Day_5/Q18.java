4e3edcbe47ceaf2300eb452e5bd96623c3ab6e84
Hamza Khan <hamzakhan@192.168.1.21>
2026-06-05 20:42:07 +0530

    Updated Day 5

---
+++ Day_5/Q18.java
@@ -0,0 +1,24 @@
+package Day_5;
+import java.util.Scanner;
+public class Q18 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int original = num;
+        int sum = 0;
+        while (num > 0) {
+            int digit = num % 10;
+            int fact = 1;
+            for (int i = 1; i <= digit; i++) {
+                fact *= i;
+            }
+            sum += fact;
+            num /= 10;
+        }
+        if (sum == original) {
+            System.out.println("Strong Number");
+        } else {
+            System.out.println("Not a Strong Number");
+        }
+    }
+}
