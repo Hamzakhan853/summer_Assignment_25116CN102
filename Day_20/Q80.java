c5d7a0bb346020074dd3ef28cd022a729ba8a33a
Hamza Khan <hamzakhan@192.168.1.75>
2026-06-20 20:54:27 +0530

    Updated Day 20

---
+++ Day_20/Q80.java
@@ -0,0 +1,25 @@
+package Day_20;
+
+import java.util.*;
+class Q80{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+
+int r=sc.nextInt();
+int c=sc.nextInt();
+
+int a[][]=new int[r][c];
+
+for(int i=0;i<r;i++)
+for(int j=0;j<c;j++)
+a[i][j]=sc.nextInt();
+
+for(int j=0;j<c;j++){
+int sum=0;
+for(int i=0;i<r;i++)
+sum+=a[i][j];
+
+System.out.println(sum);
+}
+}
+}
